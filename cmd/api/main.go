630e4e3a069952f83d84c3b166c51d7f2da2815c
Victor Toschi <victor.toschi@nuvemshop.com.br>
2024-07-22 20:44:06 -0300

    Replace factories calls by the handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,7 +9,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/Pos-tech-FIAP-GO-HORSE/order-management/internal/factories"
+	"github.com/Pos-tech-FIAP-GO-HORSE/order-management/internal/handlers"
 	"github.com/Pos-tech-FIAP-GO-HORSE/order-management/internal/infra/repositories"
 	inmemorydb "github.com/Pos-tech-FIAP-GO-HORSE/order-management/internal/infra/repositories/inmemorydb/products"
 	postgresdb "github.com/Pos-tech-FIAP-GO-HORSE/order-management/internal/infra/repositories/postgresdb/products"
@@ -61,8 +61,8 @@ func main() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	// Factories
-	productHandler := factories.MakeProductFactory(productRepository)
+	// Handlers
+	productHandler := handlers.NewProductHandler(productRepository)
 
 	app := gin.Default()
 	routes.AddProductsRoutes(app, productHandler)
