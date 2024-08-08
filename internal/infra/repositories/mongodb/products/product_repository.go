4634580f41f919dc1ea6da226865ebf57b2c44d1
Victor Toschi <victor.toschi@hotmail.com>
2024-08-08 20:55:46 -0300

    feat: create FindByCategory()

--- internal/infra/repositories/mongodb/products/product_repository.go
+++ internal/infra/repositories/mongodb/products/product_repository.go
@@ -77,6 +77,29 @@ func (p *ProductRepository) FindByID(ctx context.Context, id string) (*domain_pr
 	return &product, nil
 }
 
+func (p *ProductRepository) FindByCategory(ctx context.Context, category string) ([]*domain_products.Product, error) {
+	cursor, err := p.collection.Find(ctx, bson.M{"category": category})
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(ctx)
+
+	products := make([]*domain_products.Product, 0)
+
+	for cursor.Next(ctx) {
+		var product domain_products.Product
+		if err = cursor.Decode(&product); err != nil {
+			return nil, err
+		}
+
+		products = append(products, &product)
+	}
+
+	return products, nil
+}
+
 func (p *ProductRepository) UpdateByID(ctx context.Context, id string, product *domain_products.UpdateProduct) error {
 	objectID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
