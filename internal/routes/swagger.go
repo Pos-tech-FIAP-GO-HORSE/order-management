9d952ece0881aee4d1c7951a02a37cf94a23c14f
Victor Toschi <victor.toschi@hotmail.com>
2024-08-09 21:51:39 -0300

    feat: adjust dependecies

--- internal/routes/swagger.go
+++ internal/routes/swagger.go
@@ -1,9 +1,10 @@
 package routes
 
 import (
+	_ "github.com/Pos-tech-FIAP-GO-HORSE/order-management/cmd/docs"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func AddSwaggerRoute(app *gin.Engine) {
