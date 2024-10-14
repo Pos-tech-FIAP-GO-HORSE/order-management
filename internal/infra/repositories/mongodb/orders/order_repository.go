edcc18def564a30e50f32dd69b799f14c8f94718
Victor Toschi <victor.toschi@nuvemshop.com.br>
2024-10-14 13:58:15 -0300

    feat: get orderId from the mongodb response

--- internal/infra/repositories/mongodb/orders/order_repository.go
+++ internal/infra/repositories/mongodb/orders/order_repository.go
@@ -26,12 +26,14 @@ func NewOrderRepository(collection *mongo.Collection) repositories.IOrderReposit
 }
 
 func (o *OrderRepository) Create(ctx context.Context, order *domain_orders.Order) (domain_orders.OrderOutput, error) {
-	_, err := o.collection.InsertOne(ctx, order)
+	result, err := o.collection.InsertOne(ctx, order)
 	if err != nil {
 		return domain_orders.OrderOutput{}, err
 	}
 
-	return domain_orders.OrderOutput{ID: order.ID}, nil
+	orderID := result.InsertedID.(primitive.ObjectID)
+
+	return domain_orders.OrderOutput{ID: orderID.Hex()}, nil
 }
 
 func (o *OrderRepository) Find(ctx context.Context, filter utils.OrderFilters, offset int64, limit int64) ([]*domain_orders.Order, error) {
